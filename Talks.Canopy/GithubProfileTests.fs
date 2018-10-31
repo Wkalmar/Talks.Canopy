67b4c45e50d57bedacd0f1f0ef8701bdb83544a7
Bohdan Stupak <bstupak@acceptic.com>
2018-10-31 17:07:42 +0200

    Parallel run

--- Talks.Canopy/GithubProfileTests.fs
+++ Talks.Canopy/GithubProfileTests.fs
@@ -1,28 +1,37 @@
 module GithubProfileTests
 
-open canopy.runner.classic
-open canopy.classic
+open OpenQA.Selenium
+open canopy.parallell.functions
+open canopy.types
+open prunner
+
+
+let maximizeBrowser (browser : IWebDriver) =
+  browser.Manage().Window.Maximize()
 
 let all() =
-    context "Github page tests"
-
     "Left bottom repostitory should be staionwalk.server" &&& fun _ ->
-        url "https://github.com/Wkalmar"
-        let repo = nth 4 GithubProfilePage.pinnedRepository
-        let firstRepoCaption = repo |> someElementWithin GithubProfilePage.pinndedRepositoryContainer
+        let browser = start Chrome
+        maximizeBrowser browser
+        url "https://github.com/Wkalmar" browser
+        let repo = nth 4 GithubProfilePage.pinnedRepository browser
+        let firstRepoCaption = someElementWithin GithubProfilePage.pinndedRepositoryContainer repo browser
         match firstRepoCaption with
-        | Some caption -> read caption == "stationwalk.server"
+        | Some caption -> equals (read caption browser) "stationwalk.server" browser
         | None _ -> failwith "Element not found"
 
     "Right bottom repostitory should be staionwalk.client" &&& fun _ ->
-        url "https://github.com/Wkalmar"
-        let repo = nth 5 GithubProfilePage.pinnedRepository
-        let firstRepoCaption = repo |> someElementWithin GithubProfilePage.pinndedRepositoryContainer
+        let browser = start Chrome
+        maximizeBrowser browser
+        url "https://github.com/Wkalmar" browser
+        let repo = nth 5 GithubProfilePage.pinnedRepository browser
+        let firstRepoCaption = someElementWithin GithubProfilePage.pinndedRepositoryContainer repo browser
         match firstRepoCaption with
-        | Some caption -> read caption == "stationwalk.client"
+        | Some caption -> equals (read caption browser) "stationwalk.client" browser
         | None _ -> failwith "Element not found"
 
     "Bio should contain twitter link" &&& fun _ ->
-        url "https://github.com/Wkalmar"
-        GithubProfilePage.bio == "https://twitter.com/BohdanStupak1"
-
+        let browser = start Chrome
+        maximizeBrowser browser
+        url "https://github.com/Wkalmar" browser
+        equals GithubProfilePage.bio "https://twitter.com/BohdanStupak1" browser
--- Talks.Canopy/Program.fs
+++ Talks.Canopy/Program.fs
@@ -1,22 +1,11 @@
-﻿open canopy.configuration
-open canopy.runner.classic
-open System
-open OpenQA.Selenium
-open canopy.classic
+﻿open System
 
-let maximizeBrowser (browser : IWebDriver) =
-  browser.Manage().Window.Maximize()
-
-chromeDir <- "C:\\"
-start chrome
-
-maximizeBrowser browser
+canopy.configuration.chromeDir <- "C:\\"
 
 GithubProfileTests.all()
 
-run()
+prunner.run 3 |> ignore
 
 printfn "Press any key to exit..."
 Console.ReadKey() |> ignore
 
-quit()
